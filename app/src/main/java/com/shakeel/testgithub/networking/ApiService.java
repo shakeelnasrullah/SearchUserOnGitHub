45b91a77cee564d2bb0779f4a98be27e34a18262
Shakeel Nasrullah <shakeel.nasrullah72725@gmail.com>
2018-07-11 13:46:50 +0500

    Access Token is removed
    Authenticated user's access token will be used to call these api's now.

--- app/src/main/java/com/shakeel/testgithub/networking/ApiService.java
+++ app/src/main/java/com/shakeel/testgithub/networking/ApiService.java
@@ -14,9 +14,11 @@ import retrofit2.http.Path;
 public interface ApiService {
 
     // Auth with my generated Access Token
-    @GET("{username}?access_token=fdab43196440162b6e2dd97a00f7c5ddeebc016f")
+   // @GET("{username}?access_token=fdab4319644qewqeqwa00f7c5ddeebc016f")
+     @GET("{username}?access_token=<Your Access Token as above comment>")
     Call<User> getUser(@HeaderMap Map<String, String> userHeaders, @Path("username") String username);
 
-    @GET("{username}/followers?access_token=fdab43196440162b6e2dd97a00f7c5ddeebc016f")
+   // @GET("{username}/followers?access_token=fdab431e2dfadfafa0f7cadfaf5ddeebc016f")
+    @GET("{username}/followers?access_token=<Your Access Token as above comment>")
     Call<List<Follower>> getFollower(@HeaderMap Map<String, String> userHeaders, @Path("username") String username);
 }
