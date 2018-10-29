377692884d682c3c7322d65518bc1fadd984a1b6
Shakeel Nasrullah <shakeel.nasrullah72725@gmail.com>
2018-10-29 19:10:37 +0500

    Access Token is deleted .

--- app/src/main/java/com/shakeel/testgithub/networking/ApiService.java
+++ app/src/main/java/com/shakeel/testgithub/networking/ApiService.java
@@ -15,12 +15,11 @@ public interface ApiService {
 
     // Auth with my generated Access Token
    // @GET("{username}?access_token=fdab4319644qewqeqwa00f7c5ddeebc016f")
-     //@GET("{username}?access_token=<Your Access Token as above comment>")
-    @GET("{username}?access_token=29321df5fa37a177ccd2caa7364d3435522a328c")
+     @GET("{username}?access_token=<Your Access Token as above comment>")
+    
     Call<User> getUser(@HeaderMap Map<String, String> userHeaders, @Path("username") String username);
 
    // @GET("{username}/followers?access_token=fdab431e2dfadfafa0f7cadfaf5ddeebc016f")
-   // @GET("{username}/followers?access_token=<Your Access Token as above comment>")
-    @GET("{username}/followers?access_token=29321df5fa37a177ccd2caa7364d3435522a328c")
+    @GET("{username}/followers?access_token=<Your Access Token as above comment>")
     Call<List<Follower>> getFollower(@HeaderMap Map<String, String> userHeaders, @Path("username") String username);
 }
